4ff937e84391b00aa82c39f13c594b940c80e153
SoufianeAbk <soufiane.abakkiou@student.ehb.be>
2024-10-21 12:13:18 +0200

    persoonlijke_begroeting.mjs

---
+++ persoonlijke_begroeting.mjs
@@ -0,0 +1,10 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
+
+function begroeting(naam) {
+    console.log(`Hallo, ${naam}! Welkom!`);
+}
+
+begroeting('Souf');
+process.exit()
